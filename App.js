6db776d736f5bd7a548205a4ac6c52e14fbcab83
Vipul Sunil Patil <vipul.patil@iitgn.ac.in>
2024-08-24 04:58:40 +0530

    No

--- App.js
+++ App.js
@@ -1,35 +1,28 @@
-import { Ionicons } from '@expo/vector-icons';
-import { StatusBar } from 'expo-status-bar';
-import { ScrollView, StyleSheet, Text, View } from 'react-native';
-import useStyles from './Source/Styles/Styles';
+// In App.js in a new project
 
-export default function App() {
-  const styles = useStyles();
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+function HomeScreen() {
   return (
-    // <View style={styles.container}>
-    <ScrollView horizontal className='bg-black'>
-      <StatusBar hidden />
-      <View className="w-[1%] flex-1 items-center">
-        <Ionicons name="home" size={24} color={styles.diffBlue} />
-        <Ionicons name="albums" size={24} color='red' />
-        <Ionicons name="albums-outline" size={24} color='red' />
-      </View>
-      <View className="flex-1 items-center justify-center bg-black">
-        <Text className=' text-white'>Open up App.js to start working on your app!</Text>
-      </View>
-      <View className="flex-1 items-center justify-center bg-black">
-        <Text className=' text-white'>Open up App.js to start working on your app!</Text>
-      </View>
-    </ScrollView>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
