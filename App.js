a71cd3cba5aef99b86784812abc2f7c35c3d238b
Vipul Sunil Patil <vipul.patil@iitgn.ac.in>
2024-08-24 04:36:49 +0530

    small setup tour checks

--- App.js
+++ App.js
@@ -1,13 +1,27 @@
+import { Ionicons } from '@expo/vector-icons';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import useStyles from './Source/Styles/Styles';
 
 export default function App() {
+  const styles = useStyles();
+
   return (
     // <View style={styles.container}>
+    <ScrollView horizontal className='bg-black'>
+      <StatusBar hidden />
+      <View className="w-[1%] flex-1 items-center">
+        <Ionicons name="home" size={24} color={styles.diffBlue} />
+        <Ionicons name="albums" size={24} color='red' />
+        <Ionicons name="albums-outline" size={24} color='red' />
+      </View>
       <View className="flex-1 items-center justify-center bg-black">
-      <Text className=' text-white'>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+        <Text className=' text-white'>Open up App.js to start working on your app!</Text>
+      </View>
+      <View className="flex-1 items-center justify-center bg-black">
+        <Text className=' text-white'>Open up App.js to start working on your app!</Text>
+      </View>
+    </ScrollView>
   );
 }
 
---
+++ Source/Styles/Styles.js
@@ -0,0 +1,17 @@
+import { useColorScheme } from 'react-native';
+import React from 'react';
+
+function useStyles() {
+    const darkMode = useColorScheme() === 'dark';
+    const colors = {
+        diffBlue: '#37FEF6',
+        diffYellow: '#FFB364',
+        mainbg: darkMode ? '#000000' : '#FFFFFF',
+        subbg: '#1E2022',
+        textMain: darkMode ? '#FFFFFF' : '#000000',
+    };
+
+    return colors;
+}
+
+export default useStyles;
