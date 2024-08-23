45a11c2ef0ff1f7a7a8b3f62c4d4080df05ce6ca
Vipul Sunil Patil <vipul.patil@iitgn.ac.in>
2024-08-24 03:29:28 +0530

    added tailwind

--- App.js
+++ App.js
@@ -3,8 +3,9 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    // <View style={styles.container}>
+      <View className="flex-1 items-center justify-center bg-black">
+      <Text className=' text-white'>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
