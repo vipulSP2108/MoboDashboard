35e7238e7c4aa451a44f305e905cd621e5971943
Vipul Sunil Patil <vipul.patil@iitgn.ac.in>
2024-08-24 12:35:15 +0530

    no

--- Source/Screen/HomeScreen.js
+++ Source/Screen/HomeScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, ScrollView } from 'react-native'
+import { View, Text, ScrollView, Dimensions } from 'react-native'
 import React from 'react'
 import useColorStyle from '../Styles/ColorStyle';
 
@@ -6,14 +6,19 @@ export default function HomeScreen() {
     const colorStyle = useColorStyle();
     return (
         <ScrollView horizontal style={{ backgroundColor: colorStyle.mainbg }}>
-            <View >
+            <View className='items-center justify-center' style={{ height: Dimensions.get('window').height * 1, width: Dimensions.get('window').height * 0.9 }}>
+                <View className=" rotate-12 flex-row">
+                    <View style={{ height: Dimensions.get('window').height * 0.8, width: Dimensions.get('window').height * 0.4 }} className='overflow-hidden'>
+                        <View style={{ height: Dimensions.get('window').height * 0.8, width: Dimensions.get('window').height * 0.8 }} className=' rounded-full bg-slate-200'>
 
-            </View>
-            <View style={{ width: 800, flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <Text>Hello</Text>
-            </View>
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <Text>Hello</Text>
+                        </View>
+                    </View>
+                    <View style={{ height: Dimensions.get('window').height * 0.8, width: Dimensions.get('window').height * 0.4 }} className='rotate-180 overflow-hidden'>
+                        <View style={{ height: Dimensions.get('window').height * 0.8, width: Dimensions.get('window').height * 0.8 }} className=' rounded-full bg-slate-500'>
+
+                        </View>
+                    </View>
+                </View>
             </View>
         </ScrollView>
     )
